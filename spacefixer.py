dfdc907c1adfb3226f8dea96bfc74f22c5326494
Loy van Beek <loy.vanbeek@gmail.com>
2013-08-19 22:40:44 +0200

    Training runs successfully, although most values are very close to 0, and nearly all are the samen value.

--- spacefixer.py
+++ spacefixer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from collections import defaultdict
+from nltk.corpus import brown as corpus
+import itertools
 
 def ngrams(wordlist, n):
     """For list of words, generate sequences of n length
@@ -23,22 +25,41 @@ def probability_map(ngrams):
     0.25
     >>> prob[('c', 'd')]
     0.25
-    >>> prob[('z', 'z')]
-    0.0
     """
-    #counts = defaultdict(int)
+    counts = defaultdict(int)
     #Or: {ngram:ngrams.count(ngram) for ngram in set(ngrams)} #dict([(ngram, ngrams.count(ngram)) for ngram in set(ngrams)])
-    # for ngram in ngrams:
-    #     counts[ngram] += 1
-
-    counts = {ngram:ngrams.count(ngram) for ngram in set(ngrams)}
+    for ngram in ngrams:
+        counts[ngram] += 1
+    # ngrams_list = list(ngrams)
+    # counts = dict([(ngram, ngrams_list.count(ngram)) for ngram in set(ngrams_list)])
 
     total = float(sum(counts.values()))
 
-    probabilities = {k:(v/total) for k,v in counts.iteritems()}
+    probabilities = dict([(k, (v/total)) for k,v in counts.iteritems()])
 
-    return defaultdict(float, probabilities)
+    return probabilities
 
+
+def train(n, sentences):
+    """Perform the whole training process
+    >>> sentences = [['a', 'b', 'c'], ['b', 'c', 'd', 'e'], ['d', 'e']]
+    >>> train(2, sentences)
+    {('a', 'b'): 0.16666666666666666, ('b', 'c'): 0.33333333333333331, ('c', 'd'): 0.16666666666666666, ('d', 'e'): 0.33333333333333331}"""
+    ngrams_for_sentences = [ngrams(sent, n) for sent in sentences]
+    chained = itertools.chain(*ngrams_for_sentences)
+
+    probs = probability_map(chained)
+
+    return probs
+
+def sentence_score(sentence, training):
+    """Determine the score of a sentence against the training. 
+    This score is the probability of the ngrams multiplied, for now."""
+    sent_ngrams = ngrams(sentence)
+    ngram_probabilities = [training[ngram] for ngram in sent_ngrams]
+
+    total = reduce(lambda a,b: a*b, ngram_probabilities, 1)
+    return total
 
 
 if __name__ == "__main__":
@@ -58,4 +79,11 @@ if __name__ == "__main__":
     #       Determine probability for n-grams. 
     #   Permutation with highest probability is the fixed sentence.
 
-    pass
+    
+    #import ipdb; ipdb.set_trace()
+    sentences = corpus.sents()
+    #sentences = [['a', 'b', 'c'], ['b', 'c', 'd', 'e'], ['d', 'e']]
+    p = train(2, sentences)
+
+    import pprint
+    pprint.pprint(p)
